9be8dbb1a8804cb4362ab2f42521e76113d1f830
theo wiik <theo.wiik@itggot.se>
2017-03-21 15:19:22 +0100

    70% fungerande nav

--- Mission-Appossible/site/js/script.js
+++ Mission-Appossible/site/js/script.js
@@ -3,3 +3,20 @@ function menu_toggler() {
     element.classList.toggle("nav_is_visible")
 
 }
+
+// function menu_toggler() {
+//     document.querySelector("nav").style.width = "250px";
+//
+// }
+
+// $('#delete').click(function (e) {
+//     $("nav").addClass('animate');
+// });
+//
+// $('#add').click(function (e) {
+//     $("add").append("<div class='notification'></div>");
+// });
+//
+// $('add').on('transitionend', function(e){
+// $(e.target).remove()
+// });
